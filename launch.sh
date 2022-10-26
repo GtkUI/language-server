76a9ef6851d97c39c5578714fa454825196b5463
Zaedus <nintendozaedus@gmail.com>
2022-10-26 12:28:21 -0500

    Semi-functional start

---
+++ launch.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+SERVER_PATH=$PWD/target/debug/gtkui-language-server code --extensionDevelopmentPath=$PWD
---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult {
+            capabilities: ServerCapabilities {
+                completion_provider: Some(CompletionOptions::default()),
+                text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                    TextDocumentSyncKind::FULL
+                )),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "server initialized!")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file opened!")
+            .await;
+        self.on_change(TextDocumentItem {
+            uri: params.text_document.uri,
+            text: params.text_document.text,
+            version: params.text_document.version,
+        })
+        .await
+    }
+
+    async fn did_change(&self, mut params: DidChangeTextDocumentParams) {
+        self.on_change(TextDocumentItem {
+            uri: params.text_document.uri,
+            text: std::mem::take(&mut params.content_changes[0].text),
+            version: params.text_document.version,
+        })
+        .await
+    }
+
+    async fn completion(&self, _: CompletionParams) -> Result<Option<CompletionResponse>> {
+        Ok(Some(CompletionResponse::Array(vec![
+            CompletionItem {
+                label: "MyCoolLabel".to_string(),
+                insert_text: Some("MyCoolText".to_string()),
+                kind: Some(CompletionItemKind::VARIABLE),
+                detail: Some("MyCoolDetail".to_string()),
+                ..Default::default()
+            },
+        ])))
+    }
+}
+
+struct TextDocumentItem {
+    uri: Url,
+    text: String,
+    version: i32,
+}
+
+impl Backend {
+    async fn on_change(&self, params: TextDocumentItem) {
+        self.client
+            .log_message(MessageType::INFO, format!("{}", &params.version))
+            .await;
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, socket) = LspService::new(|client| Backend { client });
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
